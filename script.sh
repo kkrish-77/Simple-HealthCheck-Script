f0481a364026e082e42271f22cadcc8eb19b37f9
kkrish-77 <kkrshsingh@gmail.com>
2025-10-28 18:15:49 +0530

    Print outputs

--- script.sh
+++ script.sh
@@ -5,3 +5,7 @@
 CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8"%"}')
 MEM=$(free | awk '/Mem/{printf("%.2f%"), $3/$2*100}')
 DISK=$(df -h / | awk 'NR==2 {print $5}')
+
+echo "CPU Usage: $CPU"
+echo "Memory Usage: $MEM"
+echo "Disk Usage: $DISK"
